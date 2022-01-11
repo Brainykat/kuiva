35520ed49d0437ead323a2f73100caedc278fe7c
Chris <eng.christhuku@gmail.com>
2022-01-11 22:00:59 +0300

    favorite component

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.scss']
+})
+export class FavoriteComponent implements OnInit {
+
+  btnText:string="";
+  @Input() isFavorite:boolean = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+    this.btnText = this.isFavorite ? "True":"false";
+  }
+  onClick(){
+    this.isFavorite=!this.isFavorite;
+    this.btnText = this.isFavorite ? "True":"false";
+  }
+}
