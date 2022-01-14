0cc57977aac7269b0e8999ed9d9cabd178b56a1a
Chris <eng.christhuku@gmail.com>
2022-01-14 16:27:28 +0300

    Json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ export class AppComponent {
   data = [
     {id:1, name:"Oti",isFav:true},
     {id:2, name:"Saba",isFav:true},
-    {id:3, name:"Kip",isFav:false},
-    {id:4, name:"Yusuf",isFav:true},
-    {id:5, name:"Pombe",isFav:false},
-    {id:6, name:"Waru",isFav:true}
+    // {id:3, name:"Kip",isFav:false},
+    // {id:4, name:"Yusuf",isFav:true},
+    // {id:5, name:"Pombe",isFav:false},
+    // {id:6, name:"Waru",isFav:true}
 ];
 trackPerson(index:number,d:any){
   d? d.id:undefined;
