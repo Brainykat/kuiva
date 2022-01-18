e42daed681116ce05112c6cd590ac842982d1e37
Chris <eng.christhuku@gmail.com>
2022-01-18 15:14:15 +0300

    reactive forms base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+//import { FormGroup,FormControl } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
@@ -8,6 +9,7 @@ import { FavoriteComponent } from './favorite/favorite.component';
 import { LikeComponent } from './like/like.component';
 import { ZippyComponent } from './zippy/zippy.component';
 import { ContactFormComponent } from './contact-form/contact-form.component';
+import { SignupFormComponent } from './signup-form/signup-form.component';
 
 @NgModule({
   declarations: [
@@ -15,11 +17,12 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
     FavoriteComponent,
     LikeComponent,
     ZippyComponent,
-    ContactFormComponent
+    ContactFormComponent,
+    SignupFormComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    FormsModule,ReactiveFormsModule //FormControl, FormGroup
     //Validators, FormBuilder,FormControl
   ],
   providers: [],
---
+++ src/app/signup-form/signup-form.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl,Validators  } from '@angular/forms';
+
+@Component({
+  selector: 'app-signup-form',
+  templateUrl: './signup-form.component.html',
+  styleUrls: ['./signup-form.component.scss']
+})
+export class SignupFormComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  form = new FormGroup({
+    username: new FormControl('', Validators.required),
+    password:new FormControl('', Validators.required)
+  })
+}
