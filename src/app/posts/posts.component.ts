0218f4a8d34665e59077a13d0d32c95ba490c95f
Chris <eng.christhuku@gmail.com>
2022-01-19 15:22:17 +0300

    handle expected errors

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -15,18 +15,28 @@ export class PostsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.service.getPosts().subscribe(
-      response => {
-          console.log("data",response);
-      },
-      error => {
-        //This should be a Toast
-        //Log this error
-          alert('An unexpected error occured');
-          console.log(console.error);
+    this.service.getPosts()
+      .subscribe(
+        response => {
+            console.log("data",response);
+        },
+        (error:Response) => {
+          if(error.status === 400 ){
+            //Expected error bad request
+            //Serialize data to API response object 
+            //Add the errors to form
+          }
+          if(error.status === 404 ){
+            //Expected error not found
+          }
           
-      }
-    )
+          //This should be a Toast
+          //Log this error
+            alert('An unexpected error occured');
+            console.log(console.error);
+            
+        }
+      )
   }
 
 }
