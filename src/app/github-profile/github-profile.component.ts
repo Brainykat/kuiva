f1502eb8cafeacfb7087839d4385b51766b2b791
Chris <eng.christhuku@gmail.com>
2022-01-20 19:41:18 +0300

    routing simple

---
+++ src/app/github-profile/github-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-github-profile',
+  templateUrl: './github-profile.component.html',
+  styleUrls: ['./github-profile.component.scss']
+})
+export class GithubProfileComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    //If you dont allow next| prev on same page you can use snapshot
+    //this.route.snapshot.paramMap.get('id');
+    this.route.paramMap
+      .subscribe(params => {
+          //console.log(params);
+          let id = params.get('id');
+          console.log(id);
+      });
+  }
+
+}
